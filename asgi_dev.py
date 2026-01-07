f685887fef6ebad9368f3c480d49cb34809ad7eb
mahfuzdotnet <mahfuzfsl@gmail.com>
2026-01-07 23:36:28 +0600

    changes

--- asgi_dev.py
+++ asgi_dev.py
@@ -142,7 +142,8 @@ def _allowed_origins() -> list[str]:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=_allowed_origins(),
+    # allow_origins=_allowed_origins(),
+    allow_origins=["https://echoscript.ai"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
